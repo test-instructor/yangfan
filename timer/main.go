fe9fe6860dca7ce3cd50f6c17b96c436bbb2e875
yangfan <yangfanplatform@gmail.com>
2023-05-15 22:44:46 +0800

    定时任务服务抽离

---
+++ timer/main.go
@@ -0,0 +1,32 @@
+package main
+
+//go:generate go env -w GO111MODULE=on
+//go:generate go env -w GOPROXY=https://goproxy.cn,direct
+//go:generate go mod tidy
+//go:generate go mod downloa
+
+import (
+	"fmt"
+	"github.com/test-instructor/yangfan/server/core"
+	"github.com/test-instructor/yangfan/server/global"
+	"github.com/test-instructor/yangfan/server/initialize"
+	"github.com/test-instructor/yangfan/server/source/yangfan"
+	"math/rand"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	rand.Seed(time.Now().UnixNano())
+	global.GVA_VP = core.Viper()
+	global.GVA_LOG = core.Zap()
+	global.GVA_DB = initialize.Gorm()
+	yangfan.PyPkg()
+	initialize.TimerTaskCase()
+	s := <-c
+	fmt.Println("exit", s)
+}
