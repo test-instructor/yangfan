65f2cc5ca2ae3ade93adc33e121c89cca7f7d8d6
yangfan <yangfanplatform@gmail.com>
2023-06-22 02:28:08 +0800

    尝试修复docker部署无法复制问题

--- web/src/view/interface/Reports/tableKeyValue.vue
+++ web/src/view/interface/Reports/tableKeyValue.vue
@@ -49,12 +49,11 @@ const tableKeyToValue = async (data) => {
 const initData = () => {
   tableKeyToValue(props.tableData);
 };
-
+let copy_report_data = "";
 const copy = (row) => {
-  let last = JSON.stringify(row);
-  console.log(last);
+  copy_report_data = JSON.stringify(row);
   navigator.clipboard
-    .writeText(last)
+    .writeText(copy_report_data)
     .then(() => {
       console.log("复制成功");
       ElMessage.success("复制成功");
@@ -63,28 +62,6 @@ const copy = (row) => {
       console.error("复制失败", error);
       ElMessage.error("复制失败");
     });
-};
-
-const createElement = (text) => {
-  let isRTL = document.documentElement.getAttribute("dir") === "rtl";
-  let element = document.createElement("textarea");
-  // 防止在ios中产生缩放效果
-  element.style.fontSize = "12pt";
-  // 重置盒模型
-  element.style.border = "0";
-  element.style.padding = "0";
-  element.style.margin = "0";
-  // 将元素移到屏幕外
-  element.style.position = "absolute";
-  element.style[isRTL ? "right" : "left"] = "-9999px";
-  // 移动元素到页面底部
-  let yPosition = window.pageYOffset || document.documentElement.scrollTop;
-  element.style.top = `${yPosition}px`;
-  //设置元素只读
-  element.setAttribute("readonly", "");
-  element.value = text;
-  document.body.appendChild(element);
-  return element;
 };
 
 initData();
