93b1b17f9e79c28986c4251c7d2b19d083458810
yangfan <taylorchen9158@gmail.com>
2023-09-06 19:33:44 +0800

    断言调整

--- hrp/response.go
+++ hrp/response.go
@@ -195,7 +195,7 @@ func (v *responseObject) Validate(iValidators []interface{}, variablesMapping ma
 		// do assertion
 		global.GVA_LOG.Debug("assertFunc", zap.Any("checkValue", checkValue), zap.Any("expectValue", expectValue))
 		var result bool
-		if !(checkValue == nil || expectValue != nil) {
+		if checkValue != nil || expectValue != nil {
 			result = assertFunc(v.t, checkValue, expectValue)
 		}
 		if result {
