800893b43832e7fb5a9ac98417d6508a62bc09ba
van_liu <826199528@qq.com>
2023-11-14 11:48:15 +0800

    更正timer-

--- timer/main.go
+++ timer/main.go
@@ -3,7 +3,7 @@ package main
 //go:generate go env -w GO111MODULE=on
 //go:generate go env -w GOPROXY=https://goproxy.cn,direct
 //go:generate go mod tidy
-//go:generate go mod downloa
+//go:generate go mod download
 
 import (
 	"fmt"
